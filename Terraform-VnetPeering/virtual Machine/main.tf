90dc38ee3431b03201578ee9e5d57b590591f73b
Saksham Garcha <sakshamgarcha2@gmail.com>
2025-11-12 13:36:07 +0000

    updated files

--- Terraform-VnetPeering/virtual Machine/main.tf
+++ Terraform-VnetPeering/virtual Machine/main.tf
@@ -14,13 +14,14 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   features {}
   subscription_id = "7e9cd62d-d9f6-4bcd-885a-e03b46c8596c"
-  tenant_id       = "50982fd6-720f-4f84-91cb-4c68692d0983"
-  client_id       = "9a400d54-b402-455f-8118-7b46f8d80ad7"
-  client_secret   = "hXy8Q~PLOdttRfY5pYGKeZ8jynqzVk8Y3YMqRalY"
 }
 # Azure Terraform Backend
 terraform {
   backend "azurerm" {
+    use_oidc             = true  
+    use_azuread_auth     = true
+    tenant_id            = "50982fd6-720f-4f84-91cb-4c68692d0983"
+    client_id            = "376942aa-646e-4649-9b97-b244d69b27cd"
     resource_group_name  = "terraform-storage-rg"
     storage_account_name = "saksham914"
     container_name       = "tfstate"
