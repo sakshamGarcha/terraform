4acbcf24b7ed165f3592551119c7b8d5e4a55f8f
Saksham Garcha <sakshamgarcha2@gmail.com>
2025-11-12 16:28:02 +0000

    deleted

--- Terraform-VnetPeering/vnet-peering/backend.tf
+++
@@ -1,9 +0,0 @@
-# Azure Terraform Backend
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-storage-rg"
-    storage_account_name = "saksham914"
-    container_name       = "tfstate"
-    key                  = "vnet.terraform.tfstate"
-  }
-}
