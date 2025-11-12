3e7687a11eeda311b8058f3ac0a9518181640b90
Saksham Garcha <sakshamgarcha2@gmail.com>
2025-11-12 20:32:35 +0000

    sai3

--- VirtualMachine/main.tf
+++ VirtualMachine/main.tf
@@ -21,7 +21,7 @@ terraform {
     resource_group_name  = "terraform-storage-rg"
     storage_account_name = "saksham914"
     container_name       = "tfstate"
-    key                  = "virtualmachine.terraform.tfstate"
+    key                  = "vm.terraform.tfstate"
   }
 }
 
